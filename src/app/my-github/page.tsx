919428794ca18ad94646220204cc395d0f4f25af
Pedro Henrique <phcosta069@gmail.com>
2024-01-17 02:55:34 -0300

    github image include

--- src/app/my-github/page.tsx
+++ src/app/my-github/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import meImage from "../../assets/me.png"
+import githubImage from "../../assets/github.png"
 
 import { Button } from "@/components/Button"
 import Image from "next/image"
@@ -9,10 +9,14 @@ import Link from "next/link"
 export default function MyGithub() {
 
     return (
-        <section className="flex flex-col flex-1 justify-center items-center gap-20">
-            <p className="text-2xl font-bold">
-                If you liked my project and want to see more of my projects fell free to :)
-            </p>
+        <section className="flex flex-col flex-1 items-center gap-16">
+            <div className="flex flex-col items-center gap-6">
+                <Image src={githubImage} alt="githubImage" className="w-96" />
+
+                <p className="text-2xl font-bold">
+                    If you liked my project and want to see more of my projects fell free to :)
+                </p>
+            </div>
 
             <div className="flex gap-4">
                 <Link href={'https://github.com/czzped'} target="_blank">
